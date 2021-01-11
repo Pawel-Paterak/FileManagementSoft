bafa954c7546a88d9ba1f1d7c0169b888b9660ee
DESKTOP-FBCO5OS\Pawel <pawelpaterak@onet.pl>
2021-01-11 19:38:26 +0100

    #01 Added Newtonsoft.Json nuget Added FileValidate

---
+++ JsonSoft/FileValidate.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace JsonSoft
+{
+    public static class FileValidate
+    {
+        public static bool ExistsDirectory(string path)
+            => Directory.Exists(path);
+
+        public static bool ExistsFile(string path)
+            => File.Exists(path);
+
+        public static string CheckFileExtension(string file, string extension)
+           => Path.ChangeExtension(file, extension);
+    }
+}
