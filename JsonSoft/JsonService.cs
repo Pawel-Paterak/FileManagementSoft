d13ed472978b96edbd2de3df3ad3c190cd968d7b
DESKTOP-FBCO5OS\Pawel <pawelpaterak@onet.pl>
2021-01-11 21:06:04 +0100

    Added JsonService class for management reading and creating json files Added function ChangeFileExtension for FileValidate class Changed function CheckFileExtension in FileValidate

---
+++ JsonSoft/JsonService.cs
@@ -0,0 +1,31 @@
+﻿using JsonSoft.Reader;
+using JsonSoft.Writer;
+
+namespace JsonSoft
+{
+    public class JsonService
+    {
+        private readonly JsonWriter jsonWriter = new JsonWriter();
+        private readonly JsonReader jsonReader = new JsonReader();
+        private const string exenstionJson = ".Json";
+
+        public void Serialize<T>(T obj, string path) where T : class
+        {
+            if (FileValidate.ExistsFile(path))
+            {
+                FileValidate.ChangeFileExtension(ref path, exenstionJson);
+                jsonWriter.Serialize(obj, path);
+            }
+        }
+
+        public T Deserialize<T>(string path) where T : class
+        {
+            if (FileValidate.ExistsFile(path))
+            {
+                FileValidate.ChangeFileExtension(ref path, exenstionJson);
+                return jsonReader.Deserialize<T>(path);
+            }
+            return null;
+        }
+    }
+}
