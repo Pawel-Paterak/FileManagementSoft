a1d071cc821c68f416c2972875a32564caf216ba
DESKTOP-FBCO5OS\Pawel <pawelpaterak@onet.pl>
2021-01-11 20:39:52 +0100

    Fixed JsonNewtonsoft.Json Added JsonReader class for reading json files Added JsonWriter class for writing json files

---
+++ JsonSoft/Reader/JsonReader.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace JsonSoft.Reader
+{
+    public class JsonReader
+    {
+        public T Deserialize<T>(string path) where T : class
+        {
+            T temp = default;
+            using (StreamReader sR = new StreamReader(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                temp = serializer.Deserialize(sR, typeof(T)) as T;
+            }
+            return temp;
+        }
+    }
+}
---
+++ JsonSoft/Writer/JsonWriter.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+
+namespace JsonSoft.Writer
+{
+    public class JsonWriter
+    {
+        public void Serialize<T>(T obj, string path) where T : class
+        {
+            using (StreamWriter sW = new StreamWriter(path))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(sW, obj);
+            }
+        }
+    }
+}
